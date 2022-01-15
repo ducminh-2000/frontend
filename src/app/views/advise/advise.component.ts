960e66a67b71757de623e6d699b6c85bf462c601
duc08 <duc08@DESKTOP-NVG2TNG>
2022-01-15 21:35:04 +0700

    update

--- src/app/views/advise/advise.component.ts
+++ src/app/views/advise/advise.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import Industry from "../../model/Industry";
@@ -9,19 +10,38 @@ import Stock from "../../model/Stock";
   styleUrls: ["./advise.component.scss"],
 })
 export class AdviseComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) {}
   namHoaVon: number = 1;
   listStock: Stock[] = [];
   listIndustry: Industry[] = [];
+  listStockChange: Stock[] = [];
   stock: Stock = new Stock();
 
   ngOnInit(): void {
     this.getIndustry();
+    this.getStock();
   }
 
-  getIndustry() {}
+  getIndustry() {
+    this.http.get("http://localhost:5000/industry").subscribe((data: any) => {
+      console.log(data);
+      this.listIndustry = data;
+    });
+  }
 
-  getStock(id: number) {}
+  getStock() {
+    this.http.get("http://localhost:5000/stock").subscribe((data: any) => {
+      this.listStock = data;
+    });
+  }
+
+  onchangeIndustry(id: number){
+    this.listStockChange = this.listStock.filter((item) => {
+      return item.industryId === id;
+    })
+    console.log(this.listStockChange)
+  }
+
   onSubmit() {
     // this.stockService.checkStock(this.stock).subscribe((data) => {
     //   alert("Thêm thành công bản ghi");
