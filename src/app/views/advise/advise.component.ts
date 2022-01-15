e101de89065da66e4ba149659bfc468bdf450693
Thanh Loc <locdt.developer@gmail.com>
2022-01-15 22:40:51 +0700

    implement logic advice invest

--- src/app/views/advise/advise.component.ts
+++ src/app/views/advise/advise.component.ts
@@ -1,7 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import Industry from "../../model/Industry";
-import Stock from "../../model/Stock";
 
 @Component({
   selector: "app-advise",
@@ -9,30 +8,63 @@ import Stock from "../../model/Stock";
   styleUrls: ["./advise.component.scss"],
 })
 export class AdviseComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router) {}
-  namHoaVon: number = 1;
-  listStock: Stock[] = [];
-  listIndustry: Industry[] = [];
-  stock: Stock = new Stock();
+  constructor(private http: HttpClient) {}
+  pe: number = 1;
+  listStock = [];
+  rawListStock = [];
+  listIndustry = [];
+  stock: any;
+  industryId: any;
 
   ngOnInit(): void {
     this.getIndustry();
+    this.getStock();
   }
 
-  getIndustry() {}
+  getIndustry() {
+    this.http.get("http://localhost:5000/industry").subscribe((data: any) => {
+      this.listIndustry = data;
+    });
+  }
 
-  getStock(id: number) {}
+  getStock() {
+    this.http.get("http://localhost:5000/stock").subscribe((data: any) => {
+      this.rawListStock = data;
+    });
+  }
+
+  onChangeIndustry(id: number) {
+    this.listStock = this.rawListStock.filter((item) => item.industryId == id);
+    console.log(this.listStock);
+  }
+
   onSubmit() {
-    // this.stockService.checkStock(this.stock).subscribe((data) => {
-    //   alert("Thêm thành công bản ghi");
-    // }, error => {
-    //   alert("Có lỗi xảy ra, mã lỗi: " + error.status);
-    // })
-  }
-  back() {
-    this.gotoList();
-  }
-  gotoList() {
-    this.router.navigate(["/toanha/dichvu"]);
+    if (this.stock && this.stock.id) {
+      this.http
+        .post("http://localhost:5000/stock/checkStock", {
+          stockCode: this.stock.mack,
+          pe: this.pe,
+        })
+        .subscribe((data: any) => {
+          alert(data.message);
+        });
+    } else {
+      this.http
+        .post("http://localhost:5000/stock/checkListStock", {
+          industryId: this.industryId,
+          pe: this.pe,
+        })
+        .subscribe((data: any[]) => {
+          if (data && data.length == 0) {
+            alert("Không nên đầu tư ngành này trong khoảng thời gian này !");
+          } else {
+            let res = "";
+            for (let item of data) {
+              res += item.stockCode + ", ";
+            }
+            alert(`Các mã nên đầu tư là: ${res}`);
+          }
+        });
+    }
   }
 }
